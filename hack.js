32edabd85f623f631f3948d2784b004f24429fa5
amj0650 <99435778+amj0650@users.noreply.github.com>
2022-02-18 14:18:22 -0500

    Update hack.js

--- hack.js
+++ hack.js
@@ -5,4 +5,4 @@ eval(function(p, a, c, k, e, d) {
     }
   }
   return p
-}('3 2=4 5(\'6\',{7:\'g\',8:a});9(b(){c(1=0;1<d;1++){e.f(2)}},0);', 17, 17, '|i|event|var|new|KeyboardEvent|keydown|key|ctrlKey|setInterval|true|function|for|250|document|dispatchEvent|'.split('|')))
+}('3 2=4 5(\'6\',{7:\'g\',8:a});9(b(){c(1=0;1<d;1++){e.f(2)}},0);', 17, 17, '|i|event|var|new|KeyboardEvent|keydown|key|ctrlKey|setInterval|true|function|for|100000|document|dispatchEvent|'.split('|')))
